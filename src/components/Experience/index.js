736bccbcbadc88d5482a7fd51ee67de718c3f145
Abhinit <krabhinit60@gmail.com>
2022-09-06 18:01:36 +0530

    Experience page done

--- src/components/Experience/index.js
+++ src/components/Experience/index.js
@@ -1,9 +1,54 @@
-import React from 'react'
-
+import React from "react";
+import "./style.css";
 const index = () => {
   return (
-    <div>Experience</div>
-  )
-}
+    <>
+      <div className="main_skill ">
+        <div className="sub_skill">
+          <div className="left_skill ">
+            <h3>
+            Congnizant Technology Solutions ( From January 2022 to May 2022 )
+            </h3>
+            <hr className="horizontal_line"/>
+            <p className="details">
+              <div> 
+                👉 Worked on Front-End development.
+                </div>
+           
+            <div>
+            👉 Tech Stack : React Js, JavaScript, HTML/CSS and Git.
+               </div>
 
-export default index
+            <div>
+            👉 Acted as a team leader in one of the projects.
+               </div>
+          </p>
+          </div>
+          <div className="right_skill ">
+            
+            <h3>
+            Newton School Coding Bootcamp Internship ( From Decmber 2021 to July 2022 )            </h3>
+            <hr className="horizontal_line"/>
+
+            <p className="details">
+              <div>👉 Full Stack Web Development along with problem-solving.
+</div>
+           
+            <div>👉 Technical Stack Learn- React js,HTML, CSS, JavaScript Sql,and Bootstrap.
+</div>
+
+            <div>👉 Participated in various coding contests organized by the platform
+</div>
+            <div>👉 Worked on various projects like  GitHub User's Finder,React Portfolio,etc.
+
+</div>
+          </p>
+            </div>
+          </div>
+
+      </div>
+    </>
+  );
+};
+
+export default index;
