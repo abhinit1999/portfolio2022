3c45949c90ea123b5957e840d220dc3f09d8ffad
Abhinit <krabhinit60@gmail.com>
2022-09-07 09:36:46 +0530

    Project page done

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,9 +1,125 @@
-import React from 'react'
-
+import React from "react";
+import "./style.css";
 const index = () => {
   return (
-    <div>Projects</div>
-  )
-}
+    <main>
+      <div className="main_div">
+        <div className="pro1">
+          <h2>Delicious Food App</h2>
+          <hr className="horizontal_line" />
+          <p className="details">
+            <div>👉 Features: Responsiveness, beautiful Card for displaying food items.
 
-export default index
+            </div>
+
+            <div>
+              👉 State management ( Add to Cart ) with search functionality
+            </div>
+
+            <div>
+              👉 Tech Stack: React, React Router,JavaScript, HTML and CSS
+            </div>
+            <div>
+              👉 Role: Developer
+            </div>
+            <span className="link">
+              <a href="https://github.com/abhinit1999/deliciousFoodApp" target="_blank">
+                <strong>GitHub</strong>
+              </a>
+              <a href="https://delicious-food-app.vercel.app/" target="_blank">
+                <strong>LiveDemo</strong>
+              </a>
+            </span>
+          </p>
+        </div>
+        <div className="pro2">
+          <h2>EdTech App</h2>
+          <hr className="horizontal_line" />
+
+          <p className="details">
+            <div>👉 A service provider app in Education sector.</div>
+
+            <div>
+              👉 Features : One can Learn new technology like : Front-End,Android,Web Marketing etc.
+            </div>
+
+            <div>
+              👉 Tech Stack : React Js,React Router,Bootstrap,HTML,CSS.
+            </div>
+            <div>
+              👉 Role: Front-end Developer
+            </div>
+            <span className="link">
+              <a href="https://github.com/abhinit1999/learnfortomorrow-app" target="_blank">
+                <strong>GitHub</strong>
+              </a>
+              <a href="https://learnfortomorrow-by-abhinit.vercel.app/" target="_blank">
+                <strong>LiveDemo</strong>
+              </a>
+            </span>
+          </p>
+        </div>
+        <div className="pro3">
+          <h2>Food Recipes Finder App</h2>
+          <hr className="horizontal_line" />
+
+          <p className="details">
+            <div>👉 Features :  Application for searching food recipes</div>
+
+            <div>
+              👉 Most of the food Recipes are available
+            </div>
+
+            <div>
+              👉 Tech Stack: React Js , BootStrap and Edmam API
+            </div>
+            <div>
+              👉 Role: Front-End Developer
+            </div>
+            <span className="link">
+              <a href="https://github.com/abhinit1999/foodRecipies" target="_blank">
+                <strong>GitHub</strong>
+              </a>
+              <a href="https://food-recipies-by-abhinit.vercel.app/" target="_blank">
+                <strong>LiveDemo</strong>
+              </a>
+            </span>
+          </p>
+        </div>
+        <div className="pro4">
+          <h3>My Portfolio</h3>
+          <hr className="horizontal_line" />
+
+          <p className="details">
+            <div>👉 
+              Features: One can get to know more about me.
+
+            </div>
+
+            <div>
+              👉 All Projects, Skills Training / Experience available
+            </div>
+
+            <div>
+              👉 Tech Stack: React Js, React-Router HTML and CSS
+            </div>
+            <div>
+              👉 Role: Developer
+            </div>
+
+            <span className="link">
+              <a href="https://github.com/abhinit1999/portfolio2022" target="_blank">
+                <strong>GitHub</strong>
+              </a>
+              <a href="https://abhinit2022.vercel.app/" target="_blank">
+                <strong>LiveDemo</strong>
+              </a>
+            </span>
+          </p>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default index;
