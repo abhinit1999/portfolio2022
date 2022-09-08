8d6b9fdb8b67c984f99298817d3092ded76c1655
Abhinit <krabhinit60@gmail.com>
2022-09-08 15:40:42 +0530

    Footer Page done

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,7 +2,19 @@ import React from 'react'
 import "./style.css";
 const index = () => {
   return (
-    <div className='h1'>Footer</div>
+    <footer>
+    <p>Developed By: Abhinit Kumar</p>
+    <div className="follow">
+        <h4>Follow Me On </h4>
+        <a href="https://www.linkedin.com/in/abhinit-kumar-singh-325325148/" target="_blank">
+        <i className="fa fa-linkedin"></i></a>
+        <a href="https://www.facebook.com/itsAbhinit/" target="_blank">
+        <i className="fa fa-facebook"></i></a>
+  <a href="https://www.instagram.com/cse_verto/" target="_blank">      <i className="fa fa-instagram"></i></a>
+    <a href="https://twitter.com/KrAbhinit" target="_blank">    <i className="fa fa-twitter"></i></a>
+
+    </div>
+  </footer>
   )
 }
 
